24de0a428a068e305b2b90659583da93275f4849
Abdullah <khanmuhammadabdullah197@gmail.com>
2026-09-05 01:00:49 +0500

    Upload Assignment 2 with sequential file naming

---
+++ Assignment 2/q01.dart
@@ -0,0 +1,11 @@
+//Square or Rectangle7
+void main() {
+  int length = 10;
+  int breadth = 10;
+
+  if (length == breadth) {
+    print("It is a Square");
+  } else {
+    print("It is a Rectangle");
+  }
+}
